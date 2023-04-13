216c4ddfe8ce62d21f407ff94e5f661ef03e7e5b
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-04-13 23:04:23 +0100

    New name

--- Mourne-walk.py
+++ Mourne-walk.py
@@ -1,5 +1,5 @@
-import folium
-
-m = folium.Map()
-m.save("MourneWalk.html")
+#import folium
+#ocation=[-5.941592,54.184431],zoom_start=12.3)
+#m = folium.Map()
+#m.save("MourneWalk.html")
 
---
+++ Mourne_walk.py
@@ -0,0 +1,6 @@
+import folium
+
+m = folium.Map()
+m.save("MourneWalk.html")
+
+#ocation=[-5.941592,54.184431],zoom_start=12.3)
