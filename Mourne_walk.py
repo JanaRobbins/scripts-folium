3d25a354bf30d6f98578e82cfa239d84eb2375d7
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-04-16 00:17:05 +0100

    creating the map with centre in the Mourne

--- Mourne_walk.py
+++ Mourne_walk.py
@@ -1,8 +1,8 @@
 import folium
 
-#simple folium map
-m = folium.Map()
-m.save("walk.html")
+#folium map with a centre of your map (Mourne Mountain - close to Slieve Donard in this example)
+map = folium.Map(location=[-5.941592, 54.184431])
+map.save("walk.html")
 
 
 
