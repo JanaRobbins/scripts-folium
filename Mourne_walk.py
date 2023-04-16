354e3f14d90ca543000b94a11aa130685b77d516
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-04-16 15:34:12 +0100

    tiles added to the map

--- Mourne_walk.py
+++ Mourne_walk.py
@@ -2,7 +2,12 @@ import folium
 
 #folium map with a centre of your map (Mourne Mountain - close to Slieve Donard in this example)
 #the latitude is first the longitude is the second
-map = folium.Map(location=[54.184431, -5.941592], zoom=12.3)
+# zoom has to be tried to suit the required view, bigger number means more detailed map, decimal number can be used
+# default tiles are Open Street Map, but you can use Stamen Terrain, Stamen Toner, Mapbox Bright, Mapbox Control Room
+
+map = folium.Map(location=[54.184431, -5.941592], zoom_start=14, tiles="Stamen Terrain")
+
+
 map.save("walk.html")
 
 
