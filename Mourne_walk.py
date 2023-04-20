4aaf70ed0bb69e8f4651ce2062ccb04cecd00a73
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-04-20 22:51:29 +0100

    Adding weight to GeoJson
    Adding weight to GeoJson files - Polygon and Polyline

--- Mourne_walk.py
+++ Mourne_walk.py
@@ -8,13 +8,17 @@ import geopandas as gpd
 # default tiles are OpenStreetMap, but you can use Stamen Terrain, Stamen Toner, Stamen Watercolor, cartodbpositron, cartodbdark_matter, Mapbox Bright (needs api key), Mapbox Control Room (needs api key), Cloudmade (api key needed)
 #control_scale shows the scale on the map,
 
-map = folium.Map(location=[54.184431, -5.941592], control_scale='true', width='100%', left='0%', top='0%', height='100%', zoom_start=14, zoom_control=True, tiles='Stamen Terrain', name='Stamen Terrain')
+map = folium.Map(location=[54.184431, -5.941592], control_scale='true', width='100%', left='0%', top='0%', height='100%',
+                 zoom_start=14, zoom_control=True, tiles='Stamen Terrain', name='Stamen Terrain')
 
-#adding simple pop up markers - Point of Interest in this map, location lat, long and popup with the required name
+#adding custom pop up markers - Parking in this map, location lat, long and popup with the required name, html styles apply
 
 iconparking = folium.features.CustomIcon('./images/parking.png', icon_size=(50,50))
 folium.Marker (location=[54.174232, -5.873921], popup="<h2><strong>START<strong></h2><br><h4>Bloody Bridge Car Park</h4>",
                icon=iconparking).add_to(map)
+
+#adding simple pop up markers - Point of Interest in this map, location lat, long and popup with the required name
+
 folium.Marker (location=[54.170992, -5.912109], popup="<h4>Crannoge Quarry</h4>", icon=folium.Icon(color="green", icon="camera"),).add_to(map)
 folium.Marker (location=[54.172241, -5.927770], popup="<h4>Bog of Donard</h4>", icon=folium.Icon(color="green", icon="camera"),).add_to(map)
 folium.Marker (location=[54.182497, -5.945436], popup="<h4>The Castles</h4>", icon=folium.Icon(color='green', icon="camera"),).add_to(map)
@@ -27,19 +31,22 @@ folium.Marker (location=[54.209130, -5.999262], popup="<h2><strong>FINISH</stron
 
 #adding geojson layer to the map - file is in data_files/xy.geosjson folder. Location is a name of one column from the geojson file
 #Mourne Wall geojson polygon added, style1 applied, tooltip H3 added, style set to yellow with the filling color transprent =none,
+
 Mourne_wall = f"data_files/wall.geojson"
-style1={'fillColor':'none','color':'yellow'}
+style1={'fillColor':'none','color':'yellow', 'weight':'6', 'fillOpacity':'0.8'}
 folium.GeoJson(Mourne_wall, name="Mourne Wall", style_function=lambda x:style1,  tooltip="<h3>Mourne Wall</h3>").add_to(map)
 
 #Mourne paths geojson Linestring added, style1 applied, tooltip H3 added, style set to pink
+
 Mourne_paths = f"data_files/paths_all.geojson"
-style2={'fillColor':'none','color':'pink'}
+style2={'fillColor':'none','color':'pink', 'weight':'4', 'fillOpacity':'0.9'}
 folium.GeoJson(Mourne_paths, name="walking path", style_function=lambda x:style2,  tooltip="<h3>Walking path</h3>").add_to(map)
 
 
 
 #adding PolyLine to the project in this example adding one trail to the map, add the name of the line = tooltip
 # color=purple (color for the line), opacity of the line is 3
+
 trail_coordinates = [
     (54.174232, -5.873921),
     (54.170992, -5.912109),
@@ -49,13 +56,14 @@ trail_coordinates = [
     (54.190175, -5.974230),
     (54.209130, -5.999262),
 ]
-folium.PolyLine(trail_coordinates, color='purple', weight=5, opacity=1, tooltip="<h3>Bandy Pad charity walk 12 km</h3>").add_to(map)
+folium.PolyLine(trail_coordinates, color='purple', weight='5', opacity=1, tooltip="<h3>Bandy Pad charity walk 12 km</h3>").add_to(map)
 
 #Latitude/longitude popovers -  this can help users to find a location
+
 map.add_child(folium.LatLngPopup())
 
 # remove the next # if you want to see the line for the walk animated
-# plugins.AntPath(trail_coordinates).add_to(map)
+#plugins.AntPath(trail_coordinates).add_to(map)
 
 # add different types of the tiles as a base map
 
@@ -66,10 +74,12 @@ folium.raster_layers.TileLayer('Stamen Watercolor', name='Stamen Watercolor').ad
 
 
 # add layer control to show different maps
+
 folium.LayerControl().add_to(map)
 
 
 #this will save your map - you can open this map at any time and see the changes
+
 map.save("walk.html")
 
 
