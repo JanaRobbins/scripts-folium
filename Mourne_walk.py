1cf4baf131d42153e9591d84025a8be01f3fcc63
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-04-28 19:41:26 +0200

    Adding text to the map - absolute position

--- Mourne_walk.py
+++ Mourne_walk.py
@@ -1,5 +1,6 @@
 import folium
 from folium import plugins
+from folium.features import DivIcon
 import pandas as pd
 import branca
 
@@ -13,14 +14,16 @@ import branca
 map = folium.Map(location=[54.184431, -5.941592], control_scale='true', width='100%', left='0%', top='0%', height='100%',
                  zoom_start=14, zoom_control=True, tiles='Stamen Terrain', name='Stamen Terrain')
 
-#adding html into the python - box with button
-map.get_root().html.add_child(folium.Element("""
-    <div>
-    <h5>Test for html</h5><br/>
-    <button>click here...</button>
-    </div>
-    """
-                                             ))
+#adding text to the map - absolute position in the map
+folium.map.Marker(
+[54.2174, -5.8474],
+icon=DivIcon(
+icon_size=(250,50),
+icon_anchor=(0,0),
+html='<div style="font-size: 25pt">Mourne Mountain Charity Walk - 12 km</div>',
+)
+).add_to(map)
+
 
 #adding Mini Map to the map - right bottom corner
 
@@ -42,7 +45,7 @@ folium.Marker (location=[54.174232, -5.873921], tooltip="<h4>Clik here to see st
 #No.2 - adding green pop up marker with a camera icon inside - Point of Interest in this map, location lat, long and popup with html style - name and picture added,
 # tooltip added with h4 size of the text
 html2="""
-    <h3>Crannoge Quarry</h3><br/>
+    <h3>Walk up to Crannoge Quarry</h3><br/>
     <p>
     <img src="images/quarry.jpg" alt="Crannoge Quarry" class="img2">
     </p>
@@ -52,7 +55,7 @@ folium.Marker (location=[54.170992, -5.912109], tooltip="<h4>Clik here to see wh
 #No.3 - adding simple pop up markers - Point of Interest in this map, location lat, long and popup with the required name and picture,
 # tooltip added with h4 size of the text
 html3="""
-    <h3>Bof of Donard</h3><br/><h4>Crossing the wall for the first time</h4><br/>
+    <h3>Climb over Mourne Wall at Bog of Slieve Donard</h3><br/><h4>The hardest part done</h4><br/>
     <p>
     <img src="images/BogOfDonard.jpg" alt="Bog of Donard" class="img2">
     </p>
@@ -61,7 +64,7 @@ folium.Marker (location=[54.172241, -5.927770], tooltip="<h4>Clik here to see fi
 
 #No.4 - Point of Interest - next marker on the way - html style apply
 html4="""
-    <h3>The Castles</h3><br/><h4>Crossing the Mourne using Brandy Pad</h4><br/>
+    <h3>Walk under the Castles</h3><br/><h4>You can walk using Brandy Pad - smugglers crossing way over the moutanin</h4><br/>
     <p>
     <img src="images/Castles.jpg" alt="The Castles" class="img2">
     </p>
@@ -70,7 +73,7 @@ folium.Marker (location=[54.182497, -5.945436], tooltip="<h4>Clik here to see wh
 
 #No.5 - Point of Interest - next marker on the way - html style apply
 html5="""
-    <h3>View to Ben Crom Reservoir</h3><br/><h4>Enjoy the final part before walking down the hill</h4><br/>
+    <h3>Enjoy Ben Crom Reservoir view</h3><br/><h4>Enjoy the final part before walking down the hill</h4><br/>
     <p>
     <img src="images/BenCrom.jpg" alt="Ben Crom reservoir view" class="img2">
     </p>
@@ -79,7 +82,7 @@ folium.Marker (location=[54.188887, -5.962319], tooltip="<h4>Clik here to see wh
 
 #No.6- Point of Interest - next marker on the way - html style apply
 html6="""
-    <h3>Hares Gap</h3><br/><h4>Cross over the Mourne Wall for the second time</h4><br/>
+    <h3>Reaching Hares Gap walk down the hill</h3><br/><h4>Cross over the Mourne Wall for the second time</h4><br/>
     <p>
     <img src="images/HaresGap.jpg" alt="Hares Gap" class="img2">
     </p>
@@ -94,15 +97,16 @@ html7="""
     <a href=http://www.meelmorelodge.co.uk/ target=_blank</a><br/>
     <h2><strong>FINISH</strong></h2><br><h3>Meelmore Lodge</h3>
     <p>
-    <img src="images/MeelmoreLodge.jpg" alt="MeelmoreLodge" class="img2">
+    <img src="images/MeelmoreLodge.jpg" alt="Meelmore Lodge" class="img2">
     </p>
     """
 folium.Marker (location=[54.209130, -5.999262],tooltip="<h4>Clik here to see end of the walk</h4>", popup=html7,
                icon=folium.Icon(color="red", icon_color="black", icon="glyphicon glyphicon-cutlery"),).add_to(map)
 
+#navigator.geolocation.watchPosition(showPosition)
 
 #importing csv file (cracks_heading from data_files folder), using latitude and longitude as location (=column in csv file),
-#popup is a column crack_name = climbing location in the Mourens, icon set to red
+#popup is a column crack_name = climbing location in the Mournes, icon set to red
 
 pd.read_csv("./data_files/cracks_heading.csv").apply(lambda row:folium.Marker(location=[row["latitude"], row["longitude"]],
 popup=row["crack_name"], tooltip="<h4>Clik here to see name of the climbing area</h4>", icon=folium.Icon(color='red', prefix='fa fa-circle-o')).add_to(map), axis=1)
@@ -110,9 +114,10 @@ popup=row["crack_name"], tooltip="<h4>Clik here to see name of the climbing area
 
 pd.read_csv("./data_files/parking_all.csv").apply(lambda row:folium.Marker(location=[row["latitude"], row["longitude"]],
 popup=row["parking_name"], tooltip="<h4>Clik here to see parking name</h4>", icon=folium.features.CustomIcon('./images/parking.png', icon_size=(50,50))).add_to(map), axis=1)
+#TODO:change size for the crack_name
 
 #adding geojson layer to the map - file is in data_files/xy.geosjson folder. Location is a name of one column from the geojson file
-#Mourne Wall geojson polygon added, style1 applied, tooltip H3 added, style set to yellow with the filling color transprent =none,
+#Mourne Wall geojson polygon added, style1 applied, tooltip H3 added, style set to yellow with the filling color transparent =none,
 
 Mourne_wall = f"data_files/wall.geojson"
 style1={'fillColor':'none','color':'yellow', 'weight':'6', 'fillOpacity':'0.8'}
@@ -123,7 +128,6 @@ folium.GeoJson(Mourne_wall, name="Mourne Wall", style_function=lambda x:style1, 
 Mourne_paths = f"data_files/paths_all.geojson"
 style2={'fillColor':'none','color':'pink', 'weight':'4', 'fillOpacity':'0.9'}
 folium.GeoJson(Mourne_paths, name="walking path", style_function=lambda x:style2,  tooltip="<h3>Walking path</h3>").add_to(map)
-
 
 #adding PolyLine to the project in this example adding one trail to the map, add the name of the line = tooltip
 #color=purple (color for the line), opacity of the line is 1 and weight is 5
