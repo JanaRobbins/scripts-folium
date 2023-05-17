2b5fe086b7a3061c32971c65fff2b97414c5aa6d
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-05-17 22:09:12 +0100

    description corrected, images and data no longer needed deleted

--- Mourne_walk.py
+++ Mourne_walk.py
@@ -4,40 +4,41 @@ from folium import plugins
 from folium.features import DivIcon
 import pandas as pd
 import geopandas as gpd
-import branca
 
 
-#folium map with a centre of your map (Mourne Mountain - close to Slieve Donard in this example), the name is map, more descriptive than only common m
-#the latitude is first the longitude is the second
-# zoom has to be tried to suit the required view, bigger number means more detailed map, decimal number can be used, zoom control add - and + to the map,
-# default tiles are OpenStreetMap, but you can use Stamen Terrain, Stamen Toner, Stamen Watercolor, cartodbpositron, cartodbdark_matter, Mapbox Bright (needs api key), Mapbox Control Room (needs api key), Cloudmade (api key needed)
-#control_scale shows the scale on the map,
+# 1. SECTION YOU CAN CHANGE IN THIS MAP
+
+# Folium map with a centre of the map in Mourne Mountains, close to Slieve Donard in this example = location, the name set to 'map' (could be any name).
+# Control scale and zoom_control added to the map here – set for True.
+# Start zoom has to be tried to suit the required view, bigger number means more detailed map (14 in this example).
+
 
 map = folium.Map(location=[54.184431, -5.941592], control_scale='true', width='100%', left='0%', top='0%', height='100%',
                  zoom_start=14, zoom_control=True, tiles='Open Street Map', name='Open Street Map')
 
-# add different types of the tiles as a base map
+# Default tiles are OpenStreetMap, but you can use Stamen Terrain, Stamen Toner, Stamen Watercolor, cartodbpositron, cartodbdark_matter without any restriction.
+# With API key - Mapbox Bright, Mapbox Control Room, Cloudmade, Bing, Google and Thunderforest can be added.
 
 folium.raster_layers.TileLayer('Stamen Terrain', name='Stamen Terrain').add_to(map)
 folium.raster_layers.TileLayer('Stamen Toner', name='Stamen Toner').add_to(map)
 folium.raster_layers.TileLayer('Stamen Watercolor', name='Stamen Watercolor').add_to(map)
 #TODO:add another layers with API key, Google, Bing, Tunderforest
 
+# CSV file for the highest peaks in the area added using panda. Change the file to get your own data.
 df = pd.read_csv("./data_files/peaks.csv")
 
-# Create point geometries
-geometry = gpd.points_from_xy(df.Longitude, df.Latitude)
-geo_df = gpd.GeoDataFrame(
-    df[["Name", "Height", "Irish_Grid","Latitude", "Longitude", "Type"]], geometry=geometry
-)
+# Create point geometries using GeoPanda.
 
-# Create a geometry list from the GeoDataFrame
+geometry = gpd.points_from_xy(df.Longitude, df.Latitude)
+geo_df = gpd.GeoDataFrame(df[["Name", "Height", "Irish_Grid","Latitude", "Longitude", "Type"]], geometry=geometry)
+
+# Create a geometry list from the GeoDataFrame.
 geo_df_list = [[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry]
 
-# Iterate through list and add a marker for each volcano, color-coded by its type.
+# Iterate through list and add a marker for each peak, color-coded by its type.
 i = 0
 for coordinates in geo_df_list:
-    # assign a color marker for the type of volcano, Strato being the most common
+    # Assign a colour marker for the type of peak, by the column ‘type’, peaks sorted into four categories.
     if geo_df.Type[i] == "580m-630m":
         type_color = "pink"
     elif geo_df.Type[i] == "631m-700m":
@@ -49,7 +50,7 @@ for coordinates in geo_df_list:
     else:
         type_color = "blue"
 
-    # Place the markers with the popup labels and data
+    # Place the markers with the popup labels and data.
     map.add_child(
         folium.Marker(
             location=coordinates,
@@ -72,16 +73,20 @@ for coordinates in geo_df_list:
     )
     i = i + 1
 
-plugins.FloatImage('./images/legend_peaks.png', bottom=70,left=91, width='200px').add_to(map)
+# Adding legend for the peaks markers as a floating picture to the top right (number 70 and 91 are percentages from the bottom left).
 
-#adding text to the map - absolute position in the map
+plugins.FloatImage('./images/legend_peaks.png', bottom='70',left='91', width='200px').add_to(map)
+
+# Adding text to the map - absolute position in the map – DivIcon used.
+
 folium.map.Marker([54.2174, -5.8474],icon=DivIcon(icon_size=(250,50),icon_anchor=(0,0),
 html='<div style="font-size: 25pt">12 km charity walk</div>',)).add_to(map)
 
 
 #POINTS OF INTEREST ON THE WAY - Brandy Pad - NO 1 - 7 (Bloody Bridge Car Park to Meelmore Lodge)
 
-#No.1 -adding custom pop up markers - Parking in this map, location lat, long and popup with the required name, pictures added, html styles apply
+#No.1 -adding custom pop up markers - Parking in this map, location lat, long and popup with the required name, pictures added, html styles apply.
+
 html1="""
     <h2><strong>START OF THE WALK<strong></h2><br><h3>Bloody Bridge Car Park</h3>"
     <p>
@@ -92,12 +97,7 @@ iconstart = folium.features.CustomIcon('./images/Start.png', icon_size=(100,60))
 folium.Marker (location=[54.174232, -5.873921], tooltip="<h4>Clik here to see start of the walk</h4>", popup=html1,
                icon=iconstart).add_to(map)
 
-#htmls=pd.read.csv
-#pd.read.csv=("./data_files/popup26.csv").apply(popup=row["text"])
-#neco jako <br/> aby to bylo na druhem radku a row[“image”] a  alt=row[“alt”]
-#TODO: cycling the popup
-
-#No.2 - 6 Popup - HTML style for the description (h3 style apply) and a picture of the area
+# Point of interest No.2 – No.6 - HTML style for the description (h3 style apply) and a picture of the area.
 
 html2="""
     <h3>Walk up to Crannoge Quarry</h3><br/>
@@ -134,20 +134,16 @@ html6="""
     </p>
     """
 
-# Cycling through all the points of interest
+# Cycling through all the points of interest.
 
 x_coordinates = [54.170992, 54.172241, 54.182497, 54.188887, 54.190175]
 y_coordinates = [-5.912109, -5.927770, -5.945436, -5.962319, -5.974230]
 htmls = [html2, html3, html4, html5, html6]
 
 for myMarker in range(len(x_coordinates)):
-    folium.Marker(location=[x_coordinates[myMarker], y_coordinates[myMarker]],
-                  tooltip="<h4>Clik here to see the surrounding area</h4>",
-                  popup=htmls[myMarker], icon=folium.features.CustomIcon('./images/icon_green.png', icon_size=(30,50))).add_to(map)
+    folium.Marker(location=[x_coordinates[myMarker], y_coordinates[myMarker]],tooltip="<h4>Clik here to see the surrounding area</h4>", popup=htmls[myMarker], icon=folium.features.CustomIcon('./images/icon_green.png', icon_size=(30,50))).add_to(map)
 
-# color of the marker is red, icon_color is color of the symbol inside the marker - black, glyphicon is set to cutlery
-# adding url to this marker popup, this will open Meelmore Lodge in a new web window, picture added
-# FINISH is in the size h2 and bold (=strong)
+# Adding url to this marker popup, this will open Meelmore Lodge in a new web window, picture added.
 
 html7="""
     <a href=http://www.meelmorelodge.co.uk/ target=_blank</a><br/>
@@ -157,60 +153,37 @@ html7="""
     </p>
     """
 
+# Text FINISH in the html size h2 and bold(=strong) and image added into the popup. Png custom pictures used for an icon.
+
 iconfinish = folium.features.CustomIcon('./images/Finish.png', icon_size=(100,60))
 folium.Marker(location=[54.209130, -5.999262], tooltip="<h4>Clik here to see end of the walk</h4>", popup=html7, icon=iconfinish).add_to(map)
 
-# importing csv file (cracks_heading from data_files folder), using latitude and longitude as location (=column in csv file),
-# popup is a column crack_name = climbing location in the Mournes, icon set to red
-# set markers to custom png pictures
-
-#icon_climb=folium.features.CustomIcon('./images/0-250.png', icon_size=(50,50)
-#def image:
-    #if crack_elevation_m in range(0,250):
-      #  return "./images/0-250.png"
-   # elif crack_elevation_m  in range(251,400):
-       # return "./images/251-400.png"
-  #  elif crack_elevation_m  in range(401,550):
-    #    return "./images/401-550.png"
-  #  else:
-   #     return "./images/551-800.png"
-  #  return "./images/0-250.png"
-
-
-#for MyImg in range(0,len(df)):
-	#    df=df['crack_elevation_m'].iloc[i]
-	#    if  crack_elevation_m in range(0,250):
-	 #       image ='./images/0-250.png'
-     #   if crack_elevation_m in range(251, 400):
-     #       image = './images/251-400.png'
-	 #   elif crack_elevation_m in range(401,550):
-	  #      image ='./images/401-550.png'
-	 #   else:
-	    #     image ='./images/551-800.png'
+# Importing csv file (cracks_heading from data_files folder), using latitude and longitude as location (=column in csv file)
+# in popups a column crack_name = climbing location in the Mournes and crack face used, custom icon used – png picture of mountain.
 
 df=pd.read_csv("./data_files/cracks_heading.csv").apply(lambda row:folium.Marker(location=[row["latitude"], row["longitude"]],
-popup="<h3>" + row['crack_name'] + "</h3>" + '' +"<h4>" + row['crack_faces'] + "</h4>" +"<h4>" + "orientation" + "</h4>", tooltip="<h4>Clik here to see the climbing area</h4>", icon=folium.features.CustomIcon('./images/cracks.png', icon_size=(50,50))).add_to(map), axis=1)
-
+popup="<h3>" + row['crack_name'] + "</h3>" + '' +"<h4>" + row['crack_faces'] + "</h4>" +"<h4>" + "orientation" + "</h4>", tooltip="<h4>Clik here to see the climbing area</h4>",
+                                                                                 icon=folium.features.CustomIcon('./images/cracks.png', icon_size=(50,50))).add_to(map), axis=1)
+# Importing csv file (parking_all from data_files folder), custom icon used – png picture of parkings. A small anonymus function lambda is used here again.
 
 pd.read_csv("./data_files/parking_all.csv").apply(lambda row:folium.Marker(location=[row["latitude"], row["longitude"]],
-popup="<h3>" + row["parking_name"] + "</h3>", tooltip="<h4>Clik here to see parking name</h4>", icon=folium.features.CustomIcon('./images/parking.png', icon_size=(50,50))).add_to(map), axis=1)
+popup="<h3>" + row["parking_name"] + "</h3>", tooltip="<h4>Clik here to see parking name</h4>",
+                                                                           icon=folium.features.CustomIcon('./images/parking.png', icon_size=(50,50))).add_to(map), axis=1)
 
-
-# adding geojson layer to the map - file is in data_files/xy.geosjson folder. Location is a name of one column from the geojson file
-# Mourne Wall geojson polygon added, style1 applied, tooltip H3 added, style set to yellow with the filling color transparent =none,
+# Mourne Wall GeojJson polygon added from the folder data_files, style of the line set to yellow with the filling colour transparent = none.
 
 Mourne_wall = f"data_files/wall.geojson"
 style1={'fillColor':'none','color':'yellow', 'weight':'6', 'fillOpacity':'0.8'}
 folium.GeoJson(Mourne_wall, name="Mourne Wall", style_function=lambda x:style1,  tooltip="<h3>Mourne Wall</h3>").add_to(map)
 
-# Mourne paths geojson Linestring added, style1 applied, tooltip H3 added, style set to pink
+# Mourne paths GeoJson Linestring added, style1 applied, style of the paths set to pink.
 
 Mourne_paths = f"data_files/paths_all.geojson"
 style2={'fillColor':'none','color':'purple', 'weight':'1.2', 'fillOpacity':'0.8'}
 folium.GeoJson(Mourne_paths, name="walking path", style_function=lambda x:style2,  tooltip="<h3>Walking path</h3>").add_to(map)
 
-# adding PolyLine to the project in this example adding one trail to the map, add the name of the line = tooltip
-# color=purple (color for the line), opacity of the line is 1 and weight is 5
+# Adding PolyLine to the project in this example adding a waking trail to the map, add the name of the line while moving mouse over it = tooltip,
+# colour set for green (colour for the line). Coordinates could be changed for any different.
 trail_coordinates = [
     (54.174232, -5.873921),
     (54.170992, -5.912109),
@@ -222,37 +195,40 @@ trail_coordinates = [
 ]
 folium.PolyLine(trail_coordinates, color='green', weight='5', opacity=1, tooltip="<h3>Bandy Pad charity walk 12 km</h3>").add_to(map)
 
-# Latitude/longitude popovers -  this can help users to find a geolocation on the map
+# 2. SECTION YOU DO NOT NEED TO CHANGE IN THE MAP
+
+# Latitude/longitude popovers - this can help users to find a geolocation on the map by clicking anywhere in the map.
+
 my_popup = folium.LatLngPopup()
 map.add_child(my_popup)
 
-# remove the next # if you want to see the line for the walk animated
+# Remove the next # if you want to see the line for the walk animated.
 # plugins.AntPath(trail_coordinates).add_to(map)
 
-# Add a field that shows the coordinates of the mouse position on the top right
+# Add a field that shows the coordinates of the mouse position on the top right.
 fmtr = "function(num) {return L.Util.formatNum(num, 4) + ' º ';};"
 plugins.MousePosition(position="topright", separator="  //  ", num_digits=5, prefix="<h3>"+"Coordinates:"+"</h3>", lat_formatter=fmtr, lng_formatter=fmtr).add_to(map)
 
-# measure control added - check your position during walk and how long do you have to walk to reach another destination
+# Measure control added - check your position during walk and how long do you have to walk to reach another destination.
 folium.plugins.MeasureControl(position='topleft', active_color='red', completed_color='red').add_to(map)
 
-# control plugin to geolocate the user
+# Control plugin to geolocate the user - check your position during the walk!!!
 plugins.LocateControl().add_to(map)
 
-# adding a floating image in HTML canvas on buttom left of the map with **kwargs - additional keyword argument as CSS properties
+# Adding a floating image in HTML canvas on bottom left of the map with **kwargs - additional keyword argument as CSS properties – your logo can be here.
 plugins.FloatImage('./images/j.png', bottom=6,left=1, width='60px').add_to(map)
 
-# full screen button in the map
+# Full screen button in the map.
 plugins.Fullscreen(force_separate_button=True, title='Click here to see Full Screen').add_to(map)
 
-# adding Mini Map to the map - right bottom corner
+# Adding Mini Map to the map - right bottom corner.
 plugins.MiniMap(width='300',height='300').add_to(map)
 
-# a search box was added
+# A search box was added.
 folium.plugins.Geocoder().add_to(map)
 
-# add layer control to show different maps
+# Add layer control to show different maps.
 folium.LayerControl().add_to(map)
 
-# this will save your map - you can open this map from the finder at any time and see the changes
+# This will save your map - you can open this map from the finder at any time and see the changes.
 map.save("walk.html")
